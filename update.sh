b07f8dba5e6c82d9022e91a2d0e6427e498edc90
Paris Holley <mail@parisholley.com>
2013-08-22 14:12:44 -0500

    make sure it don't delete itself

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 rm Berksfile.lock
-ls | grep -v Berksfile | xargs rm -rf
+ls | grep -v Berksfile | grep -v Berksfile.lock  | grep -v update.sh | xargs rm -rf
 mkdir /tmp/opsworksberk
 berks install --path=/tmp/opsworksberk --except aws
 ls /tmp/opsworksberk/ | grep -v ^php$ | xargs -Ixxx cp -rf /tmp/opsworksberk/xxx .
