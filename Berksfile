4df278c8c6791357c647811359c9ed11c807b54a
Paris Holley <mail@parisholley.com>
2013-08-22 14:11:47 -0500

    aws provides php, shouldn't need it, just ignore it when pulled from berks

--- Berksfile
+++ Berksfile
@@ -1,6 +1,6 @@
-group :aws do
-	cookbook "php"
-end
+#group :aws do
+#	cookbook "php", git: "https://github.com/aws/opsworks-cookbooks.git", branch: "master-chef-11.4", rel: "php"
+#end
 
 cookbook "newrelic", git: "https://github.com/escapestudios/chef-newrelic.git"
 cookbook "apc", git: "https://github.com/teamsnap/php-apc-cookbook.git"
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 rm Berksfile.lock
-ls | grep -v Berksfile | grep -v ^php$ | rm -rf
+ls | grep -v Berksfile | xargs rm -rf
 mkdir /tmp/opsworksberk
-berks install --path=/tmp/opsworksberk --without aws
-cp -rf /tmp/opsworksberk/* .
+berks install --path=/tmp/opsworksberk --except aws
+ls /tmp/opsworksberk/ | grep -v ^php$ | xargs -Ixxx cp -rf /tmp/opsworksberk/xxx .
