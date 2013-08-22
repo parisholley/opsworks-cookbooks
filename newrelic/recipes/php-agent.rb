331849b04135a020d2832a87a9a43a0dd156e048
Paris Holley <mail@parisholley.com>
2013-08-22 18:51:43 -0500

    trial and error

--- newrelic/recipes/php-agent.rb
+++ newrelic/recipes/php-agent.rb
@@ -6,7 +6,7 @@
 #
 
 include_recipe "php"
-include_recipe "apache2::service"
+include_recipe "apache2"
 
 #the older version (3.0) had a bug in the init scripts that when it shut down the daemon it would also kill dpkg as it was trying to upgrade
 #let's remove the old packages before continuing
