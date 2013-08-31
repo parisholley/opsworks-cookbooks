b75e392973ae7f8fde641cb11a932216d050ea0d
Paris Holley <mail@parisholley.com>
2013-08-31 13:32:59 -0600

    fixed

--- newrelic/recipes/meetme-deploy.rb
+++ newrelic/recipes/meetme-deploy.rb
@@ -1,4 +1,6 @@
-execute "copy-apc-status" do
-    command "cp /opt/newrelic_plugin_agent/apc-nrp.php #{deploy[:deploy_to]}/current/"
-    action :run
+node[:deploy].each do |application, deploy|
+	execute "copy-apc-status" do
+	    command "cp /opt/newrelic_plugin_agent/apc-nrp.php #{deploy[:deploy_to]}/current/"
+	    action :run
+	end
 end
