9d88f36599278a6dab8cd3728157920b38eabd5e
Paris Holley <mail@parisholley.com>
2013-09-01 12:26:06 -0600

    first try at nginx

---
+++ wordpress/recipes/config.rb
@@ -0,0 +1,32 @@
+include_recipe 'nginx'
+
+packages = [
+    'php5-fpm',
+    'php-xml',
+    'php-common',
+    'php-xmlrpc',
+    'php-devel',
+    'php-gd',
+    'php-cli',
+    'php-pear-Auth-SASL',
+    'php-mysql',
+    'php-mcrypt',
+    'php-pecl-memcache',
+    'php-pear',
+    'php-pear-XML-Parser',
+    'php-pear-Mail-Mime',
+    'php-pear-DB',
+    'php-pear-HTML-Common',
+    'curl-devel'
+];
+
+packages.each do |pkg|
+    package pkg do
+        action :install
+    end
+end
+
+execute "pecl-http" do
+    command "pecl install pecl_http"
+    action :run
+end
---
+++ wordpress/recipes/deploy.rb
@@ -0,0 +1,18 @@
+include_recipe 'dependencies'
+
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_user do
+    deploy_data deploy
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
